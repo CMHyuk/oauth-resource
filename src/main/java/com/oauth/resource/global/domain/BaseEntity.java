aae96e8afd7e8f78c122c711cfb9257a7ccfbbea
choi min hyuk <minhyeok.choi@softcamp.co.kr>
2024-08-20 16:43:32 +0900

    feat: BaseEntity 생성, 수정 날짜 추가

--- src/main/java/com/oauth/resource/global/domain/BaseEntity.java
+++ src/main/java/com/oauth/resource/global/domain/BaseEntity.java
@@ -1,11 +1,18 @@
 package com.oauth.resource.global.domain;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.oauth.resource.global.util.References;
 import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.elasticsearch.annotations.Field;
+import org.springframework.data.elasticsearch.annotations.FieldType;
 
 import java.io.Serializable;
+import java.time.LocalDateTime;
 
 @Getter
 @MappedSuperclass
@@ -16,4 +23,14 @@ public class BaseEntity implements Serializable {
 
     @Id
     private String id;
+
+    @CreatedDate
+    @Field(type = FieldType.Date,  format = {}, pattern = References.TIME_FORMAT)
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = References.TIME_FORMAT)
+    private LocalDateTime insertTime;
+
+    @LastModifiedDate
+    @Field(type = FieldType.Date, format = {}, pattern = References.TIME_FORMAT)
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = References.TIME_FORMAT)
+    private LocalDateTime updateTime;
 }
