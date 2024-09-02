d5f9d28befbe00110d6bb3b72e271846829732bf
choi min hyuk <minhyeok.choi@softcamp.co.kr>
2024-09-02 16:35:47 +0900

    fix: /api/name 경로 permitAll로 수정

--- src/main/java/com/oauth/resource/domain/auth/SecurityConfig.java
+++ src/main/java/com/oauth/resource/domain/auth/SecurityConfig.java
@@ -37,7 +37,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
         return http.authorizeHttpRequests(
-                        request -> request.requestMatchers("/api/master/**", "/api/version").permitAll()
+                        request -> request.requestMatchers("/api/master/**", "/api/version", "/api/name").permitAll()
                                 .anyRequest().authenticated()
                 )
                 .formLogin(AbstractHttpConfigurer::disable)
