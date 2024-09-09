8928db1cb2d72b58ac0e896e5d3a6f0576e77ed6
choi min hyuk <minhyeok.choi@softcamp.co.kr>
2024-09-09 14:54:51 +0900

    test: 삭제 후 데이터 유무 검증 추가

--- src/test/java/com/oauth/resource/resource/domain/DeleteAcceptanceTest.java
+++ src/test/java/com/oauth/resource/resource/domain/DeleteAcceptanceTest.java
@@ -1,6 +1,9 @@
 package com.oauth.resource.resource.domain;
 
+import com.oauth.resource.domain.client.repository.ClientInfoRepository;
 import com.oauth.resource.domain.token.dto.AccessTokenDeleteRequest;
+import com.oauth.resource.domain.token.repository.ElasticSearchTokenRepository;
+import com.oauth.resource.domain.user.repository.UserInfoRepository;
 import com.oauth.resource.support.ResourceAcceptanceTest;
 import com.oauth.resource.support.TestClassesOrder;
 import com.oauth.resource.support.TokenContext;
@@ -8,6 +11,7 @@ import io.restassured.RestAssured;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.*;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -16,9 +20,19 @@ import org.springframework.http.MediaType;
 import static com.oauth.resource.support.DocumentFieldConstants.CLIENT_ID;
 import static com.oauth.resource.support.DocumentFieldConstants.USER_ID;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 @TestClassesOrder(11)
 public class DeleteAcceptanceTest extends ResourceAcceptanceTest {
+
+    @Autowired
+    private ClientInfoRepository clientInfoRepository;
+
+    @Autowired
+    private UserInfoRepository userInfoRepository;
+
+    @Autowired
+    private ElasticSearchTokenRepository elasticSearchTokenRepository;
 
     @Nested
     @DisplayName("마스터 클라이언트를")
@@ -34,7 +48,10 @@ public class DeleteAcceptanceTest extends ResourceAcceptanceTest {
                     .extract();
 
             // then
-            assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
+            assertAll(
+                    () -> assertThat(clientInfoRepository.findByClientId(CLIENT_ID)).isEmpty(),
+                    () -> assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value())
+            );
         }
     }
 
@@ -52,7 +69,10 @@ public class DeleteAcceptanceTest extends ResourceAcceptanceTest {
                     .extract();
 
             // then
-            assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
+            assertAll(
+                    () -> assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value()),
+                    () -> assertThat(userInfoRepository.findByUserId(USER_ID)).isEmpty()
+            );
         }
     }
 
@@ -75,7 +95,10 @@ public class DeleteAcceptanceTest extends ResourceAcceptanceTest {
                     .extract();
 
             // then
-            assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
+            assertAll(
+                    () -> assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value()),
+                    () -> assertThat(elasticSearchTokenRepository.findByAccessToken(request.accessToken())).isEmpty()
+            );
         }
     }
 }
