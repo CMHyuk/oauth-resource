1d61518ded97de9c9533ead90bc2364c55ef755f
choi min hyuk <minhyeok.choi@softcamp.co.kr>
2024-09-09 15:14:52 +0900

    test: 중복 로직 클래스로 추출

---
+++ src/test/java/com/oauth/resource/authorization/TokenProvider.java
@@ -0,0 +1,157 @@
+package com.oauth.resource.authorization;
+
+import com.github.dockerjava.api.exception.InternalServerErrorException;
+import com.oauth.resource.domain.authorization.exception.OAuth2AuthorizationErrorCode;
+import com.oauth.resource.domain.authorization.model.CustomOAuth2Authorization;
+import com.oauth.resource.domain.authorization.repository.CustomOAuth2AuthorizationBaseRepository;
+import com.oauth.resource.global.exception.BusinessException;
+import com.oauth.resource.support.AuthorizationAcceptanceTest;
+import io.restassured.RestAssured;
+import io.restassured.http.Cookies;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+import lombok.RequiredArgsConstructor;
+import org.apache.commons.codec.binary.Base64;
+import org.elasticsearch.index.query.BoolQueryBuilder;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+
+import java.net.URI;
+import java.util.Arrays;
+import java.util.Optional;
+
+import static com.oauth.resource.support.DocumentFieldConstants.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.springframework.http.MediaType.APPLICATION_FORM_URLENCODED_VALUE;
+
+@RequiredArgsConstructor
+public class TokenProvider extends AuthorizationAcceptanceTest {
+
+    private final CustomOAuth2AuthorizationBaseRepository customOAuth2AuthorizationBaseRepository;
+
+    private Cookies cookies;
+    private String code;
+
+    public String issueToken(String username, String password) {
+        loginPageTest();
+        loginTest(username, password);
+        consentPageTest();
+        getCodeTest();
+        return getTokenTest();
+    }
+
+    private void loginPageTest() {
+        // when
+        ExtractableResponse<Response> response = RestAssured.given().log().all()
+                .get("/authorization/login")
+                .then().log().all()
+                .extract();
+
+        // then
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
+    }
+
+    private void loginTest(String username, String password) {
+        // when
+        ExtractableResponse<Response> response = RestAssured.given().log().all()
+                .queryParam("username", username)
+                .queryParam("password", password)
+                .post("/authorization/login")
+                .then().log().all()
+                .extract();
+
+        // then
+        cookies = response.detailedCookies();
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.FOUND.value());
+    }
+
+    private void consentPageTest() {
+        // when
+        ExtractableResponse<Response> response = RestAssured.given().log().all()
+                .queryParam("response_type", "code")
+                .queryParam("client_id", CLIENT_ID)
+                .queryParam("scope", SCOPE)
+                .queryParam("redirect_uri", REDIRECT_URI)
+                .cookies(cookies)
+                .get("/authorization/oauth2/authorize")
+                .then().log().all()
+                .extract();
+
+        // then
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
+    }
+
+    private void getCodeTest() {
+        // given
+        CustomOAuth2Authorization customOAuth2Authorization = getCustomOAuth2Authorization()
+                .orElseThrow(() -> BusinessException.from(OAuth2AuthorizationErrorCode.NOT_FOUND));
+
+        // when
+        ExtractableResponse<Response> response = RestAssured.given().log().all()
+                .contentType(APPLICATION_FORM_URLENCODED_VALUE)
+                .formParam("client_id", CLIENT_ID)
+                .formParam("state", customOAuth2Authorization.getState())
+                .formParam("scope", SCOPE)
+                .cookies(cookies)
+                .post("/authorization/oauth2/authorize")
+                .then().log().all()
+                .extract();
+
+        // then
+        code = getCode(response.header("Location"));
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.FOUND.value());
+    }
+
+    private String getTokenTest() {
+        // given
+        String credentials = CLIENT_ID + ":" + CLIENT_SECRET;
+        String encodedCredentials = Base64.encodeBase64String(credentials.getBytes());
+
+        // when
+        ExtractableResponse<Response> response = RestAssured.given().log().all()
+                .header("Authorization", "Basic " + encodedCredentials)
+                .contentType(APPLICATION_FORM_URLENCODED_VALUE)
+                .formParam("code", code)
+                .formParam("grant_type", "authorization_code")
+                .formParam("redirect_uri", REDIRECT_URI)
+                .cookies(cookies)
+                .post("/authorization/oauth2/token")
+                .then().log().all()
+                .extract();
+
+        // then
+        assertAll(
+                () -> assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value()),
+                () -> assertThat(response.body().jsonPath().getString("access_token")).isNotNull(),
+                () -> assertThat(response.body().jsonPath().getString("refresh_token")).isNotNull(),
+                () -> assertThat(response.body().jsonPath().getString("scope")).isEqualTo(SCOPE),
+                () -> assertThat(response.body().jsonPath().getString("token_type")).isEqualTo("Bearer"),
+                () -> assertThat(response.body().jsonPath().getString("expires_in")).isEqualTo("299")
+        );
+
+        return response.body().jsonPath().getString("access_token");
+    }
+
+    private String getCode(String locationHeader) {
+        try {
+            URI uri = new URI(locationHeader);
+            String query = uri.getQuery();
+            return Arrays.stream(query.split("&"))
+                    .filter(param -> param.startsWith("code="))
+                    .map(param -> param.split("=")[1])
+                    .findFirst()
+                    .orElse(null);
+        } catch (Exception e) {
+            throw new InternalServerErrorException(e.getMessage());
+        }
+    }
+
+    private Optional<CustomOAuth2Authorization> getCustomOAuth2Authorization() {
+        BoolQueryBuilder query = QueryBuilders.boolQuery()
+                .filter(QueryBuilders.matchQuery("code.keyword", "EMPTY_CODE"));
+        CustomOAuth2Authorization customOAuth2Authorization = customOAuth2AuthorizationBaseRepository.find(null, query);
+        return Optional.ofNullable(customOAuth2Authorization);
+    }
+}
--- src/test/java/com/oauth/resource/authorization/UserOAuth2TokenTest.java
+++ src/test/java/com/oauth/resource/authorization/UserOAuth2TokenTest.java
@@ -1,32 +1,13 @@
 package com.oauth.resource.authorization;
 
-import com.oauth.resource.domain.authorization.exception.OAuth2AuthorizationErrorCode;
-import com.oauth.resource.domain.authorization.model.CustomOAuth2Authorization;
 import com.oauth.resource.domain.authorization.repository.CustomOAuth2AuthorizationBaseRepository;
-import com.oauth.resource.global.exception.BusinessException;
 import com.oauth.resource.support.AuthorizationAcceptanceTest;
 import com.oauth.resource.support.TestClassesOrder;
 import com.oauth.resource.support.TokenContext;
-import io.restassured.RestAssured;
-import io.restassured.http.Cookies;
-import io.restassured.response.ExtractableResponse;
-import io.restassured.response.Response;
-import org.apache.commons.codec.binary.Base64;
-import org.elasticsearch.index.query.BoolQueryBuilder;
-import org.elasticsearch.index.query.QueryBuilders;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.util.Arrays;
-import java.util.Optional;
 
 import static com.oauth.resource.support.DocumentFieldConstants.*;
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertAll;
-import static org.springframework.http.MediaType.APPLICATION_FORM_URLENCODED_VALUE;
 
 @TestClassesOrder(9)
 public class UserOAuth2TokenTest extends AuthorizationAcceptanceTest {
@@ -34,123 +15,10 @@ public class UserOAuth2TokenTest extends AuthorizationAcceptanceTest {
     @Autowired
     private CustomOAuth2AuthorizationBaseRepository customOAuth2AuthorizationBaseRepository;
 
-    private Cookies cookies;
-    private String code;
-
     @Test
-    void 일반_유저가_토큰을_발행한다() throws URISyntaxException {
-        loginPageTest();
-        loginTest();
-        consentPageTest();
-        getCodeTest();
-        getTokenTest();
-    }
-
-    private void loginPageTest() {
-        // when
-        ExtractableResponse<Response> response = RestAssured.given().log().all()
-                .get("/authorization/login")
-                .then().log().all()
-                .extract();
-
-        // then
-        assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
-    }
-
-    private void loginTest() {
-        // when
-        ExtractableResponse<Response> response = RestAssured.given().log().all()
-                .queryParam("username", USER_ID)
-                .queryParam("password", USER_PASSWORD)
-                .post("/authorization/login")
-                .then().log().all()
-                .extract();
-
-        // then
-        cookies = response.detailedCookies();
-        assertThat(response.statusCode()).isEqualTo(HttpStatus.FOUND.value());
-    }
-
-    private void consentPageTest() {
-        // when
-        ExtractableResponse<Response> response = RestAssured.given().log().all()
-                .queryParam("response_type", "code")
-                .queryParam("client_id", CLIENT_ID)
-                .queryParam("scope", SCOPE)
-                .queryParam("redirect_uri", REDIRECT_URI)
-                .cookies(cookies)
-                .get("/authorization/oauth2/authorize")
-                .then().log().all()
-                .extract();
-
-        // then
-        assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
-    }
-
-    private void getCodeTest() throws URISyntaxException {
-        // given
-        CustomOAuth2Authorization customOAuth2Authorization = getCustomOAuth2Authorization()
-                .orElseThrow(() -> BusinessException.from(OAuth2AuthorizationErrorCode.NOT_FOUND));
-
-        // when
-        ExtractableResponse<Response> response = RestAssured.given().log().all()
-                .contentType(APPLICATION_FORM_URLENCODED_VALUE)
-                .formParam("client_id", CLIENT_ID)
-                .formParam("state", customOAuth2Authorization.getState())
-                .formParam("scope", SCOPE)
-                .cookies(cookies)
-                .post("/authorization/oauth2/authorize")
-                .then().log().all()
-                .extract();
-
-        // then
-        code = getCode(response.header("Location"));
-        assertThat(response.statusCode()).isEqualTo(HttpStatus.FOUND.value());
-    }
-
-    private void getTokenTest() {
-        // given
-        String credentials = CLIENT_ID + ":" + CLIENT_SECRET;
-        String encodedCredentials = Base64.encodeBase64String(credentials.getBytes());
-
-        // when
-        ExtractableResponse<Response> response = RestAssured.given().log().all()
-                .header("Authorization", "Basic " + encodedCredentials)
-                .contentType(APPLICATION_FORM_URLENCODED_VALUE)
-                .formParam("code", code)
-                .formParam("grant_type", "authorization_code")
-                .formParam("redirect_uri", REDIRECT_URI)
-                .cookies(cookies)
-                .post("/authorization/oauth2/token")
-                .then().log().all()
-                .extract();
-
-        // then
-        TokenContext.setUserAccessToken(response.body().jsonPath().getString("access_token"));
-        assertAll(
-                () -> assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value()),
-                () -> assertThat(response.body().jsonPath().getString("access_token")).isNotNull(),
-                () -> assertThat(response.body().jsonPath().getString("refresh_token")).isNotNull(),
-                () -> assertThat(response.body().jsonPath().getString("scope")).isEqualTo(SCOPE),
-                () -> assertThat(response.body().jsonPath().getString("token_type")).isEqualTo("Bearer"),
-                () -> assertThat(response.body().jsonPath().getString("expires_in")).isEqualTo("299")
-        );
-    }
-
-    private String getCode(String locationHeader) throws URISyntaxException {
-        URI uri = new URI(locationHeader);
-        String query = uri.getQuery();
-        return Arrays.stream(query.split("&"))
-                .filter(param -> param.startsWith("code="))
-                .map(param -> param.split("=")[1])
-                .findFirst()
-                .orElse(null);
-    }
-
-    private Optional<CustomOAuth2Authorization> getCustomOAuth2Authorization() {
-        BoolQueryBuilder query = QueryBuilders.boolQuery()
-                .filter(QueryBuilders.matchQuery("code.keyword", "EMPTY_CODE"));
-        CustomOAuth2Authorization customOAuth2Authorization = customOAuth2AuthorizationBaseRepository.find(null, query);
-        return Optional.ofNullable(customOAuth2Authorization);
+    void 일반_유저가_토큰을_발행한다() {
+        TokenProvider tokenProvider = new TokenProvider(customOAuth2AuthorizationBaseRepository);
+        String accessToken = tokenProvider.issueToken(USER_ID, USER_PASSWORD);
+        TokenContext.setUserAccessToken(accessToken);
     }
 }
